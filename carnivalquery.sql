54f3982bfc54d3f522ab3115d72c873c85c740b9
Matthew Hedges <matthedges35@gmail.com>
2023-06-03 21:58:57 -0500

    trigger chapter complete

--- carnivalquery.sql
+++ carnivalquery.sql
@@ -411,6 +411,46 @@ CREATE TRIGGER new_sale_made
   ON sales
   FOR EACH ROW
   EXECUTE PROCEDURE set_pickup_date();
---Create a trigger for updates to the Sales table. 
---If the pickup date is on or before the purchase date, set the pickup date to 7 days after the purchase date. 
+
+ 
+ -- 2. Create a trigger for updates to the Sales table. 
+      --If the pickup date is on or before the purchase date, set the pickup date to 7 days after the purchase date. 
+      --If the pickup date is after the purchase date but less than 7 days out from the purchase date, add 4 additional days to the pickup date.
+
+	
+CREATE OR REPLACE FUNCTION update_sales_dates()
+	RETURNS TRIGGER AS $$
+	BEGIN
+	    IF NEW.pickup_date <= NEW.purchase_date THEN
+        	NEW.pickup_date := NEW.purchase_date + INTERVAL '7 days';
+        	UPDATE sales SET pickup_date = NEW.pickup_date WHERE sales.sale_id = NEW.sale_id;
+    	END IF;
+	    
+	    IF NEW.pickup_date > NEW.purchase_date AND NEW.pickup_date < NEW.purchase_date + INTERVAL '7 days' THEN
+	        NEW.pickup_date := NEW.pickup_date + INTERVAL '4 days';
+	        UPDATE sales SET pickup_date = NEW.pickup_date WHERE sales.sale_id = NEW.sale_id;
+	    END IF;
+	    
+	    RETURN NEW;
+	END;
+$$ LANGUAGE plpgsql;
+
+CREATE or replace TRIGGER update_sales_dates_trigger
+	AFTER UPDATE ON sales
+	FOR EACH ROW
+	EXECUTE FUNCTION update_sales_dates();
+	
+
+-- pickup date is on same date as purchase date
+UPDATE public.sales
+	SET pickup_date='2023-06-04'
+	WHERE sale_id=5004;
+
+-- pick up date is after purchase date
+UPDATE public.sales
+	SET pickup_date='2023-06-10'
+	WHERE sale_id=5004;
+
+
+select * from sales where sale_id=5004;
 --If the pickup date is after the purchase date but less than 7 days out from the purchase date, add 4 additional days to the pickup date.
